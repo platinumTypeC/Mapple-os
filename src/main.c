02e7a9a822d334982b84c307247f37394a66a4c4
platinumTypeC <randomeeerandom@gmail.com>
2022-02-06 00:23:04 +0530

    Basic bootloader

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include "include/efi.h"
 #include "include/efilib.h"
 
+#include "kernel.h"
+
 EFI_STATUS
 EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
@@ -9,12 +11,16 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
     EFI_STATUS Status;
 
+    ST = SystemTable;
+
     EFI_INPUT_KEY key;
 
     uefi_call_wrapper(ST->ConOut->OutputString, 2, ST->ConOut, L"Mapple os");
     uefi_call_wrapper(ST->ConIn->Reset, 2, ST->ConIn, FALSE);
 
     while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &key)) == EFI_NOT_READY);
+
+    kmain(); // Unreachable for now
 
     return Status;
 }
