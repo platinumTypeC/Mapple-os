66d1590021020c93d4de07e93ef0fa8933a3fb0f
platinumTypeC <randomeeerandom@gmail.com>
2022-02-05 23:53:02 +0530

    Started making the bootloader

--- src/main.c
+++ src/main.c
@@ -3,15 +3,18 @@
 
 EFI_STATUS
 EFIAPI
-efi_main(
-    EFI_HANDLE ImageHandle,
-    EFI_SYSTEM_TABLE* SystemTable
-) {
+efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+    InitializeLib(ImageHandle, SystemTable);
+
     EFI_STATUS Status;
-    EFI_INPUT_KEY Key;
 
-    ST = SystemTable;
-    Print(L"Hello World\n");
+    EFI_INPUT_KEY key;
 
-    return EFI_SUCCESS;
+    uefi_call_wrapper(ST->ConOut->OutputString, 2, ST->ConOut, L"Mapple os");
+    uefi_call_wrapper(ST->ConIn->Reset, 2, ST->ConIn, FALSE);
+
+    while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &key)) == EFI_NOT_READY);
+
+    return Status;
 }
