dfbba455976c17c6f97c79f9979d72ec9605c824
platinumTypeC <randomeeerandom@gmail.com>
2022-02-06 00:40:52 +0530

    Simple Graphics Output Protocol Boot

--- src/main.c
+++ src/main.c
@@ -8,19 +8,38 @@ EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     InitializeLib(ImageHandle, SystemTable);
+    EFI_STATUS status;
+    // EFI_INPUT_KEY Key;
 
-    EFI_STATUS Status;
+    EFI_GUID gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+    
+    status = uefi_call_wrapper(BS->LocateProtocol, 3, &gopGuid, NULL, (void**)&gop);
+    if(EFI_ERROR(status))
+        Print(L"Unable to locate GOP\n");
+    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *info;
+    UINTN SizeOfInfo, numModes, nativeMode;
+    
+    status = uefi_call_wrapper(gop->QueryMode, 4, gop, gop->Mode==NULL?0:gop->Mode->Mode, &SizeOfInfo, &info);
+    // this is needed to get the current video mode
+    if (status == EFI_NOT_STARTED)
+        status = uefi_call_wrapper(gop->SetMode, 2, gop, 0);
+    if(EFI_ERROR(status)) {
+        Print(L"Unable to get native mode\n");
+    } else {
+        nativeMode = gop->Mode->Mode;
+        numModes = gop->Mode->MaxMode;
+    }
 
-    ST = SystemTable;
-
-    EFI_INPUT_KEY key;
-
-    uefi_call_wrapper(ST->ConOut->OutputString, 2, ST->ConOut, L"Mapple os");
-    uefi_call_wrapper(ST->ConIn->Reset, 2, ST->ConIn, FALSE);
-
-    while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &key)) == EFI_NOT_READY);
-
-    kmain(); // Unreachable for now
-
-    return Status;
+    for (int i = 0; i < numModes; i++) {
+        status = uefi_call_wrapper(gop->QueryMode, 4, gop, i, &SizeOfInfo, &info);
+        Print(L"mode %03d width %d height %d format %x%s\n",
+            i,
+            info->HorizontalResolution,
+            info->VerticalResolution,
+            info->PixelFormat,
+            i == nativeMode ? "(current)" : ""
+        );
+    }
+    return status;
 }
