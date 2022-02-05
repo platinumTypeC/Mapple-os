f506433b8024c563da1c51a40d83f7c8de8de4ef
platinumTypeC <randomeeerandom@gmail.com>
2022-02-05 14:06:19 +0530

    Typo

--- src/main.c
+++ src/main.c
@@ -3,11 +3,15 @@
 
 EFI_STATUS
 EFIAPI
-efi_mai(
+efi_main(
     EFI_HANDLE ImageHandle,
     EFI_SYSTEM_TABLE* SystemTable
 ) {
-    InitializeLib(ImageHandle, SystemTable);
-    Print(L"Hello World! \n");
+    EFI_STATUS Status;
+    EFI_INPUT_KEY Key;
+
+    ST = SystemTable;
+    Print(L"Hello World\n");
+
     return EFI_SUCCESS;
 }
