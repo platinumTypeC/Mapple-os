8e132dd7b0998bbf32a88214d16c8b98f7e699cd
platinumTypeC <randomeeerandom@gmail.com>
2022-02-14 10:39:58 +0530

    Fixed memory map

--- src/bootloader/boot.c
+++ src/bootloader/boot.c
@@ -11,51 +11,56 @@
 #include "include/loader.h"
 #include "include/config.h"
 
-typedef struct s_boot_info {
-	EFI_MEMORY_DESCRIPTOR* memory_map;
-	UINTN memory_map_size;
-	UINTN memory_map_descriptor_size;
-	UINTN FrameBufferBaseAddress;
-} Kernel_Boot_Info;
 
-EFI_STATUS get_memory_map(
-	OUT EFI_MEMORY_DESCRIPTOR** memory_map,
-	OUT UINTN* memory_map_size,
-	OUT UINTN* memory_map_key,
-	OUT UINTN* descriptor_size,
-	OUT UINT32* descriptor_version)
-{
-	/** Program status. */
-	EFI_STATUS status;
+EFI_STATUS
+get_memory_map(
+	OUT EFI_MEMORY_DESCRIPTOR** out_memory_map,
+	OUT UINT32* out_descripter_version,
+	OUT UINTN* out_descriptor_size,
+	OUT UINTN* out_memory_map_size,
+	OUT UINTN* out_map_key
+){
+    EFI_STATUS status = EFI_SUCCESS;
 
-	status = uefi_call_wrapper(gBS->GetMemoryMap, 5,
-		memory_map_size, *memory_map, memory_map_key,
-		descriptor_size, descriptor_version);
-	if(EFI_ERROR(status)) {
-		if(status != EFI_BUFFER_TOO_SMALL) {
-			Print(L"Fatal Error: Error getting memory map size: %s\n",
-				get_efi_error_message(status));
+    EFI_MEMORY_DESCRIPTOR *memory_map = NULL;
+    UINT32 version = 0;
+    UINTN map_key = 0;
+    UINTN descriptor_size = 0;
+    UINTN memory_map_size = 0;
+
+    status = uefi_call_wrapper(BS->GetMemoryMap, 5, &memory_map_size, memory_map, &map_key, &descriptor_size, &version);
+    // descriptor size and version should be correct values.  memory_map_size should be needed size.
+
+    if (status == EFI_BUFFER_TOO_SMALL) {
+        UINTN encompassing_size = memory_map_size + (2 * descriptor_size);
+        void *buffer = NULL;
+        status = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, encompassing_size, &buffer);
+        if (status == EFI_SUCCESS) {
+            memory_map = (EFI_MEMORY_DESCRIPTOR*) buffer;
+            memory_map_size = encompassing_size;
+
+            status = uefi_call_wrapper(BS->GetMemoryMap, 5, &memory_map_size, memory_map, &map_key, &descriptor_size, &version);
+            if (status != EFI_SUCCESS) {
+                Print(L"Second call to GetMemoryMap failed for some reason.");
+			}
 		}
-
-
-	#if MAPPLE_DEBUG != 0
-		Print(L"Debug: Allocating memory map\n");
-	#endif
-
-	*memory_map = (EFI_MEMORY_DESCRIPTOR*)AllocatePool((*memory_map_size) + 2 * (*descriptor_size));
-	CHECKER(
-		uefi_call_wrapper(gBS->GetMemoryMap, 5,
-			memory_map_size, *memory_map, memory_map_key,
-			descriptor_size, descriptor_version),
-		
-		L"Error: Unable to Get Memory Map, error: %s\n\r"
-	);
-
-	Print(L"Debug: Allocated memory map buffer at: 0x%llx "
-		L"with size: %llu\n", *memory_map, *memory_map_size);	}
+	}
+	
+	*out_memory_map = memory_map;
+	*out_descripter_version = version;
+	*out_descriptor_size = descriptor_size;
+	*out_memory_map_size = memory_map_size;
+	*out_map_key = map_key;
 
 	return EFI_SUCCESS;
-};
+}
+
+typedef struct s_boot_info {
+	EFI_MEMORY_DESCRIPTOR* memory_map;
+	UINT64 FrameBufferBaseAddress;
+	UINTN memory_map_size;
+	UINTN memory_map_descriptor_size;
+} Kernel_Boot_Info;
 
 /**
  * 
@@ -98,27 +103,28 @@ efi_main(
 	Print(L"Debug: Geting Memory Map and Info\n");
 #endif
 
-	CHECKER(
-		get_memory_map(&memory_map, &memory_map_size,
-			&memory_map_key, &descriptor_size, &descriptor_version),
-		
-		L"Error: while geting the memory map, error: %s\n"
-	);
+	get_memory_map(&memory_map, &descriptor_version, &descriptor_size, &memory_map_size, &memory_map_key);
 
 #if MAPPLE_DEBUG != 0
 	Print(L"Exting Booting Service into kernel...\n");
 #endif
+	
+	kernel_entry = (void (*)(Kernel_Boot_Info*))*KernelEnteryPoint;
 
-	CHECKER(
-		uefi_call_wrapper(gBS->ExitBootServices, 2,
-			ImageHandle, memory_map_key),
-		
-		L"Unable to exit Bootservices, error: %s\n"
-		
-	);
 
+	boot_info.FrameBufferBaseAddress = get_farmeBuffer();
+
+	// There is a status error but it works. so I will Probably re write this later
+	gBS->ExitBootServices(ImageHandle, memory_map_key);
+
+	boot_info.memory_map = memory_map;
+	boot_info.memory_map_size = memory_map_size;
+	boot_info.memory_map_descriptor_size = descriptor_size;
+
+	Print(L"Debug: Entring kernel\n");
+	kernel_entry(&boot_info);
 #if MAPPLE_DEBUG != 0 
-	Print(L"Debug: This should never be reached\n\r");
+	Print(L"Debug: This Here means your kernel is faulty..\n\r");
 #endif
 
     return EFI_SUCCESS;
