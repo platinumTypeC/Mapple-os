46d431d7e69cafdf8758397f6f07e7fb4a8e7bdd
platinumTypeC <randomeeerandom@gmail.com>
2022-02-19 22:33:38 +0530

    Now using c++ instead of c

--- src/kernel/entry/kernel.c
+++
@@ -1,8 +0,0 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <mapple/mapple.h>
-
-void kernel_main(BootInfo_t* boot_info) {
-	while(1);
-}
--- src/kernel/include/mapple/mapple.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <mapple/bootInfo.h>
-#include <mapple/config.h>
-#include <mapple/graphics.h>
