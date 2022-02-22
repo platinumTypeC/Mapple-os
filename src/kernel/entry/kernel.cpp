fea5bb2d8260c5d65a903f409c31304f3aa9d69f
platinumTypeC <randomeeerandom@gmail.com>
2022-02-22 21:37:08 +0530

    Started on Initializing the kernel!

--- src/kernel/entry/kernel.cpp
+++ src/kernel/entry/kernel.cpp
@@ -1,10 +1,21 @@
 #include <mapple/types.h>
+#include <mapple/types.hpp>
+#include <mapple/Memory.h>
+#include <cstddef>
 
 extern "C" void kernel_main(
     BootInfo_t* boot_info
 ){
-    while (true)
     {
-        asm("hlt");
-    }  
+        BasicRenderer_t renderer = BasicRenderer_t(boot_info->frameBuffer, &boot_info->defaultFont);
+        SetGlobalRenderer(&renderer);
+    }
+
+    GDTDescriptor DescTableItem;
+    DescTableItem.Size = sizeof(GDT) - 1;
+    DescTableItem.Offset = (uint64_t)&DefaultGDT;
+
+    LoadGDT(&DescTableItem);
+
+    asm("hlt");
 };
