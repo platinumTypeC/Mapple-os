b684220d99be1777b85d68bbcb55385017d5760e
platinumTypeC <randomeeerandom@gmail.com>
2022-02-22 15:36:15 +0530

    Finished remaking the bootloader

--- src/bootloader/boot.c
+++ src/bootloader/boot.c
@@ -8,13 +8,14 @@
  * @copyright Copyright (c) 2022
  * 
  */
+
 #include <efi.h>
 #include <efilib.h>
 #include <efiapi.h>
 #include "include/elf.h"
 #include "include/error.h"
-#include <mapple/config.h>
 #include <mapple/types.h>
+#include <mapple/config.h>
 
 EFI_HANDLE m_IH;
 EFI_SYSTEM_TABLE* m_ST = NULL;
@@ -23,12 +24,6 @@ EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* SimpleFsProtocol = NULL;
 EFI_GRAPHICS_OUTPUT_PROTOCOL* gGOPProtocol = NULL;
 
 BootInfo_t gbootInfo;
-
-#if MAPPLE_DEBUG != 0
-	#define DebugPrint(...) Print(L"Debug: "__VA_ARGS__)
-#else
-	#define DebugPrint(...)
-#endif
 
 EFI_STATUS
 EFIAPI
@@ -52,58 +47,69 @@ InitFileSystem(){
 
 EFI_STATUS
 EFIAPI
-LoadFile(
-	CHAR16* FileName,
-	EFI_PHYSICAL_ADDRESS *FileAddr,
-	UINTN *FilePageSize
+OpenFile(
+	IN CHAR16* FileName,
+	OUT EFI_FILE** File
 ){
-	DebugPrint(L"Starting to Load File\n");
-	EFI_FILE_PROTOCOL *File;
 	CHECKER(
-		uefi_call_wrapper(RTFileSystem->Open, 5, RTFileSystem, &File, 
+		uefi_call_wrapper(RTFileSystem->Open, 5, RTFileSystem, File, 
 			FileName, EFI_FILE_MODE_READ, 0)
 		,
 		L"Unable to Open File path !, error: %s\n"
 	);
+	DebugPrint(L"Opened File\n");
 
-  	UINTN FileInfoBufferSize = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * StrLen(FileName) + 2;
-  	UINT8 FileInfoBuffer[FileInfoBufferSize];
+	return EFI_SUCCESS;
+};
 
-  	CHECKER(
-		uefi_call_wrapper(File->GetInfo, 4, File, &gEfiFileInfoGuid, 
-			&FileInfoBufferSize, FileInfoBuffer)
-		,
-		L"Unable to get File Info !, error: %s\n"
-	);
-
-	DebugPrint(L"Got file Info\n");
-  
-	EFI_FILE_INFO *FileInfo = (EFI_FILE_INFO*)FileInfoBuffer;
-	UINTN FileSize = FileInfo->FileSize;
-
-
-	DebugPrint(L"FileSize=%llu\n",FileSize);
-	*FilePageSize = (FileSize + 4095) / 4096;
-	*FileAddr = 0;
+EFI_STATUS
+EFIAPI
+LoadFont(){
+	// Get Font file and load it into memory
+	EFI_FILE* FontFile;
 
 	CHECKER(
-		uefi_call_wrapper(BS->AllocatePages, 4, AllocateAnyPages, 
-			EfiLoaderData, *FilePageSize, FileAddr)
+		OpenFile(FONT_FILE_PATH, &FontFile)
 		,
-		L"Unable to Allocate Pages for File !, error: %s\n"
+		L"Unable to OpenFile !, error: %s\n"
 	);
 
-	DebugPrint(L"Allocated pages for File\n");
+	PSF1_HEADER_t* fontHeader = (PSF1_HEADER_t*)AllocatePool(sizeof(PSF1_FONT_t));
+
+	UINTN size = sizeof(PSF1_HEADER_t);
 
 	CHECKER(
-		uefi_call_wrapper(File->Read, 3, File, &FileSize,
-    		(VOID *)*FileAddr)
+		uefi_call_wrapper(FontFile->Read, 3, FontFile, 
+			&size, fontHeader)
 		,
-		L"Unable to Read File !, error: %s\n"
+		L"Unable to Read FontFile !, error: %s\n"
 	);
 
-	DebugPrint(L"Done Reading File\n");
+	UINTN glyphBufferSize = fontHeader->charsize * 256;
+	if (fontHeader->mode == 1) { //512 glyph mode
+		glyphBufferSize = fontHeader->charsize * 512;
+	}
 
+	void* glyphBuffer;
+	{
+		CHECKER(
+			uefi_call_wrapper(FontFile->SetPosition, 2, 
+				FontFile, sizeof(PSF1_HEADER))
+			,
+			L"Unable to Set Position of Font !, error: %s\n"
+		);
+
+		glyphBuffer = AllocatePool(glyphBufferSize);
+
+		CHECKER(
+			uefi_call_wrapper(FontFile->Read, 3, FontFile, 
+				&glyphBufferSize, glyphBuffer)
+			,
+			L"Unable to Read FontFile !, error: %s\n"
+		);
+	}
+	gbootInfo.defaultFont.glyphBuffer = glyphBuffer;
+	gbootInfo.defaultFont.psf1_Header = fontHeader;
 	return EFI_SUCCESS;
 };
 
@@ -157,14 +163,8 @@ IniGetMemoryDescriptor(){
 
     if (status == EFI_BUFFER_TOO_SMALL) {
         UINTN encompassing_size = memory_map_size + (2 * descriptor_size);
-        void *buffer = NULL;
+        void *buffer = AllocatePool(encompassing_size);
         
-		CHECKER(
-			uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, encompassing_size, &buffer)
-			,
-			L"Unable to Allocate Pool for memoryMap !, error: %s\n"
-		);
-
 		memory_map = (EFI_MEMORY_DESCRIPTOR*) buffer;
 		memory_map_size = encompassing_size;
 
@@ -226,6 +226,235 @@ InitailizeAllProtocols(
 		L"Unable to get Memory Deskriptor !, error: %s"
 	);
 	DebugPrint(L"Done Geting Memory Descriptor\n");
+	DebugPrint(L"Loading Default Font\n");
+	CHECKER(
+		LoadFont()
+		,
+		L"Unable to Load Font File !, error: %s\n"
+	);
+	DebugPrint(L"Done Loading Default Font\n");
+	return EFI_SUCCESS;
+};
+
+/**
+ * load_segment
+ */
+EFI_STATUS load_segment(IN EFI_FILE* const kernel_img_file,
+	IN EFI_PHYSICAL_ADDRESS const segment_file_offset,
+	IN UINTN const segment_file_size,
+	IN UINTN const segment_memory_size,
+	IN EFI_PHYSICAL_ADDRESS const segment_virtual_address)
+{
+	/** Program status. */
+	EFI_STATUS status;
+	/** Buffer to hold the segment data. */
+	VOID* program_data = NULL;
+	/** The amount of data to read into the buffer. */
+	UINTN buffer_read_size = 0;
+	/** The number of pages to allocate. */
+	UINTN segment_page_count = EFI_SIZE_TO_PAGES(segment_memory_size);
+	/** The memory location to begin zero filling empty segment space. */
+	EFI_PHYSICAL_ADDRESS zero_fill_start = 0;
+	/** The number of bytes to zero fill. */
+	UINTN zero_fill_count = 0;
+
+	#ifdef DEBUG
+		DebugPrint(L"Debug: Setting file pointer to segment "
+			"offset '0x%llx'\n", segment_file_offset);
+	#endif
+
+	status = uefi_call_wrapper(kernel_img_file->SetPosition, 2,
+		kernel_img_file, segment_file_offset);
+	if(EFI_ERROR(status)) {
+		DebugPrint(L"Error: Error setting file pointer position to "
+			L"segment offset: %s\n", get_efi_error_message(status));
+
+		return status;
+	}
+
+	#ifdef DEBUG
+		DebugPrint(L"Debug: Allocating %lu pages at address '0x%llx'\n",
+			segment_page_count, segment_virtual_address);
+	#endif
+
+	status = uefi_call_wrapper(gBS->AllocatePages, 4,
+		AllocateAnyPages, EfiLoaderData, segment_page_count,
+		(EFI_PHYSICAL_ADDRESS*)segment_virtual_address);
+	if(EFI_ERROR(status)) {
+		DebugPrint(L"Error: Error allocating pages for ELF segment: %s\n",
+			get_efi_error_message(status));
+
+		return status;
+	}
+
+	if(segment_file_size > 0) {
+		buffer_read_size = segment_file_size;
+
+		#ifdef DEBUG
+			DebugPrint(L"Debug: Allocating segment buffer with size '0x%llx'\n",
+				buffer_read_size);
+		#endif
+
+		status = uefi_call_wrapper(gBS->AllocatePool, 3,
+			EfiLoaderCode, buffer_read_size, (VOID**)&program_data);
+		if(EFI_ERROR(status)) {
+			DebugPrint(L"Error: Error allocating kernel segment buffer: %s\n",
+				get_efi_error_message(status));
+
+			return status;
+		}
+
+		#ifdef DEBUG
+			DebugPrint(L"Debug: Reading segment data with file size '0x%llx'\n",
+				buffer_read_size);
+		#endif
+
+		status = uefi_call_wrapper(kernel_img_file->Read, 3,
+			kernel_img_file, &buffer_read_size, (VOID*)program_data);
+		if(EFI_ERROR(status)) {
+			DebugPrint(L"Error: Error reading segment data: %s\n",
+				get_efi_error_message(status));
+
+			return status;
+		}
+
+		#ifdef DEBUG
+			DebugPrint(L"Debug: Copying segment to memory address '0x%llx'\n",
+				segment_virtual_address);
+		#endif
+
+		status = uefi_call_wrapper(gBS->CopyMem, 3,
+			segment_virtual_address, program_data, segment_file_size);
+		if(EFI_ERROR(status)) {
+			DebugPrint(L"Error: Error copying program section "
+				L"into memory: %s\n", get_efi_error_message(status));
+
+			return status;
+		}
+
+		#ifdef DEBUG
+			DebugPrint(L"Debug: Freeing program section data buffer\n");
+		#endif
+
+		status = uefi_call_wrapper(gBS->FreePool, 1, program_data);
+		if(EFI_ERROR(status)) {
+			DebugPrint(L"Error: Error freeing program section: %s\n",
+				get_efi_error_message(status));
+
+			return status;
+		}
+	}
+
+	// As per ELF Standard, if the size in memory is larger than the file size
+	// the segment is mandated to be zero filled.
+	// For more information on Refer to ELF standard page 34.
+	zero_fill_start = segment_virtual_address + segment_file_size;
+	zero_fill_count = segment_memory_size - segment_file_size;
+
+	if(zero_fill_count > 0) {
+		#ifdef DEBUG
+			DebugPrint(L"Debug: Zero-filling %llu bytes at address '0x%llx'\n",
+				zero_fill_count, zero_fill_start);
+		#endif
+
+		status = uefi_call_wrapper(gBS->SetMem, 3,
+			zero_fill_start, zero_fill_count, 0);
+		if(EFI_ERROR(status)) {
+			DebugPrint(L"Error: Error zero filling segment: %s\n",
+				get_efi_error_message(status));
+
+			return status;
+		}
+	}
+
+	return EFI_SUCCESS;
+};
+
+/**
+ * load_program_segments
+ */
+EFI_STATUS load_program_segments(IN EFI_FILE* const kernel_img_file,
+	IN Elf_File_Class const file_class,
+	IN VOID* const kernel_header_buffer,
+	IN VOID* const kernel_program_headers_buffer)
+{
+	/** Program status. */
+	EFI_STATUS status;
+	/** The number of program headers. */
+	UINT16 n_program_headers = 0;
+	/** The number of segments loaded. */
+	UINT16 n_segments_loaded = 0;
+	/** Program section iterator. */
+	UINTN p = 0;
+
+	if(file_class == ELF_FILE_CLASS_32) {
+		n_program_headers = ((Elf32_Ehdr*)kernel_header_buffer)->e_phnum;
+	} else if(file_class == ELF_FILE_CLASS_64) {
+		n_program_headers = ((Elf64_Ehdr*)kernel_header_buffer)->e_phnum;
+	}
+
+	// Exit if there are no executable sections in the kernel image.
+	if(n_program_headers == 0) {
+		DebugPrint(L"Fatal Error: No program segments to load ");
+		DebugPrint(L"in Kernel image\n");
+
+		return EFI_INVALID_PARAMETER;
+	}
+
+	#ifdef DEBUG
+		DebugPrint(L"Debug: Loading %u segments\n", n_program_headers);
+	#endif
+
+
+	if(file_class == ELF_FILE_CLASS_32) {
+		/** Program headers pointer. */
+		Elf32_Phdr* program_headers = (Elf32_Phdr*)kernel_program_headers_buffer;
+
+		for(p = 0; p < n_program_headers; p++) {
+			if(program_headers[p].p_type == PT_LOAD) {
+				status = load_segment(kernel_img_file,
+					program_headers[p].p_offset,
+					program_headers[p].p_filesz,
+					program_headers[p].p_memsz,
+					program_headers[p].p_vaddr);
+				if(EFI_ERROR(status)) {
+					// Error has already been printed in the case that loading an
+					// individual segment failed.
+					return status;
+				}
+
+				// Increment the number of segments lodaed.
+				n_segments_loaded++;
+			}
+		}
+	} else if(file_class == ELF_FILE_CLASS_64) {
+		/** Program headers pointer. */
+		Elf64_Phdr* program_headers = (Elf64_Phdr*)kernel_program_headers_buffer;
+
+		for(p = 0; p < n_program_headers; p++) {
+			if(program_headers[p].p_type == PT_LOAD){
+				status = load_segment(kernel_img_file,
+					program_headers[p].p_offset,
+					program_headers[p].p_filesz,
+					program_headers[p].p_memsz,
+					program_headers[p].p_vaddr);
+				if(EFI_ERROR(status)) {
+					return status;
+				}
+
+				n_segments_loaded++;
+			}
+		}
+	}
+
+	// If we have found no loadable segments, raise an exception.
+	if(n_segments_loaded == 0) {
+		DebugPrint(L"Fatal Error: No loadable program segments ");
+		DebugPrint(L"found in Kernel image\n");
+
+		return EFI_NOT_FOUND;
+	}
+
 	return EFI_SUCCESS;
 };
 
@@ -246,15 +475,62 @@ efi_main(
 	InitializeLib(ImageHandle, SystemTable);
 	InitailizeAllProtocols(ImageHandle, SystemTable);
 
-	// Get Font file and load it into memory
-	EFI_PHYSICAL_ADDRESS FontFileMemLocation;
-	UINTN FontFilePageBufferSize;
-	
+	EFI_FILE* kernelFile;
+
 	CHECKER(
-		LoadFile(FONT_FILE_PATH, &FontFileMemLocation, &FontFilePageBufferSize)
+		OpenFile(KERNEL_EXECUTABLE_PATH, &kernelFile)
 		,
-		L"Unable to Load Font File !, error: %s\n"
+		L"Unable to load kernel.efi !, error: %s\n"
 	);
-	
+
+
+	UINT8* elf_identity_buffer = NULL;
+	CHECKER(
+		read_elf_identity(kernelFile, &elf_identity_buffer)
+		,
+		L"Unable to read elf identity !, error: %s\n"
+	);
+
+	Elf_File_Class file_class = elf_identity_buffer[EI_CLASS];
+	CHECKER(
+		validate_elf_identity(elf_identity_buffer)
+		,
+		L"ELF identity of kernel.elf is not valid 0x10IMPOSIBLE !, error: %s\n"
+	);
+
+	FreePool(elf_identity_buffer);
+
+	VOID* kernel_header = NULL;
+	VOID* kernel_program_headers = NULL;
+	CHECKER(
+		read_elf_file(kernelFile, file_class, &kernel_header, &kernel_program_headers)
+		,
+		L"Unable to read elf from kernel file\n"
+	);
+
+	EFI_PHYSICAL_ADDRESS* kernel_entry_point = NULL;
+	if(file_class == ELF_FILE_CLASS_32) {
+		*kernel_entry_point = ((Elf32_Ehdr*)kernel_header)->e_entry;
+	} else if(file_class == ELF_FILE_CLASS_64) {
+		*kernel_entry_point = ((Elf64_Ehdr*)kernel_header)->e_entry;
+	}
+
+	CHECKER(
+		load_program_segments(kernelFile, file_class,
+			kernel_header, kernel_program_headers)
+		,
+		L"while Loding the kernel segments !, error: %s\n"
+	);
+
+	DebugPrint("Entering the kernel\n");
+
+	void (*kernel_entry)(BootInfo*);
+	kernel_entry = (void (*)(BootInfo_t*))*kernel_entry_point;
+
+	kernel_entry(&gbootInfo);
+
+	DebugPrint(L"This here should never be Reached\n");
+	DebugPrint(L"This also means that the kernel is faulty\n");
+
 	return EFI_SUCCESS;
 };
