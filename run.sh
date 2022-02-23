de78f713edd3cd493597e5a0d242f5d1cb2fb0b0
platinumTypeC <randomeeerandom@gmail.com>
2022-02-23 17:45:53 +0530

    Runner which detects wsl on its own

---
+++ run.sh
@@ -0,0 +1,6 @@
+if [[ $(grep Microsoft /proc/version) ]];
+then 
+    qemu-system-x86_64.exe -drive file=Mapple.img,format=raw -m 200M -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="OVMFbin/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="OVMFbin/OVMF_VARS-pure-efi.fd" ;
+else 
+    qemu-system-x86_64 -drive file=Mapple.img,format=raw -m 200M -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="OVMFbin/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="OVMFbin/OVMF_VARS-pure-efi.fd";
+fi
