7f2e3247ade80362db71ec446118fcd8d4a88415
platinumTypeC <randomeeerandom@gmail.com>
2022-02-24 22:57:39 +0530

    Simple test script to debug BaseAddr which had some probleams

---
+++ src/kernel/Gui/common.cpp
@@ -0,0 +1,49 @@
+#include <mapple/Gui/common.h>
+#include <stddef.h>
+
+#define TEST_SCREEN_PRIMARY_COLOUR 0x00FF4000
+#define TEST_SCREEN_SECONDARY_COLOUR 0x00FF80BF
+
+void draw_rect(
+    Framebuffer_t frameBuffer,
+    uint16_t _x,
+    uint16_t _y,
+    uint16_t width,
+    uint16_t height,
+    uint32_t color
+){
+	/** Pointer to the current pixel in the buffer. */
+	uint32_t* at;
+
+	uint16_t row = 0;
+	uint16_t col = 0;
+	for(row = 0; row < height; row++) {
+		for(col = 0; col < width; col++) {
+			at = (uint32_t*)frameBuffer.BaseAddress + _x + col;
+			at += ((_y + row) * frameBuffer.Width);
+			*at = color;
+		}
+	}
+}
+
+void testScene(
+    Framebuffer_t frameBuffer
+){
+    uint16_t tile_width = frameBuffer.Width /
+		4;
+    uint16_t tile_height = frameBuffer.Height /
+		3;
+
+	uint8_t p = 0;
+	for(p = 0; p < 12; p++) {
+		uint8_t _x = p % 4;
+		uint8_t _y = p / 4;
+
+		uint32_t color = TEST_SCREEN_PRIMARY_COLOUR;
+		if(((_y % 2) + _x) % 2) {
+			color = TEST_SCREEN_SECONDARY_COLOUR;
+		}
+
+		draw_rect(frameBuffer, tile_width * _x, tile_height * _y, tile_width, tile_height, color);
+	}
+};
---
+++ src/kernel/include/mapple/Gui/common.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <mapple/types.h>
+
+void testScene(Framebuffer_t frameBuffer);
