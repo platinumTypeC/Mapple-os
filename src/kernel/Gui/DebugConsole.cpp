60e92057285155ecc3d65d415aa809a93e3fc812
platinumTypeC <randomeeerandom@gmail.com>
2022-02-26 21:58:04 +0530

    Slight start

--- src/kernel/Gui/DebugConsole.cpp
+++ src/kernel/Gui/DebugConsole.cpp
@@ -1,5 +1,15 @@
 #include <mapple/Gui.h>
 
-void Print(char* string, PSF1_FONT_t Font){
-    
+DebugConsole::DebugConsole(Framebuffer_t* frmaebuffer, PSF1_FONT_t* NewPSF1Font){
+    TargetFramebuffer = frmaebuffer;
+    PSF1Font = NewPSF1Font;
+    Color = 0xffffffff;
+    CursorPosition = {10, 10};
+};
+
+// void DebugConsole::Print(const char* str){    
+// };
+
+void DebugConsole::putPix(uint64_t X, uint64_t Y, uint64_t Color){
+    *(uint32_t*)((uint64_t)TargetFramebuffer->BaseAddress + (X*4) + (Y * TargetFramebuffer->PixelsPerScanLine * 4)) = Color;
 };
