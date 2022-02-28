7141c140930cb064025b95291ea8d660aeb69672
platinumTypeC <randomeeerandom@gmail.com>
2022-02-28 21:33:29 +0530

    WIP DebugPrint Function for kernel

--- src/kernel/include/mapple/Gui/DebugConsole.h
+++ src/kernel/include/mapple/Gui/DebugConsole.h
@@ -10,7 +10,14 @@ class DebugConsole
         Point_t CursorPosition;
         
         DebugConsole(Framebuffer_t* frmaebuffer, PSF1_FONT_t* NewPSF1Font);
-        // void Print(const char* str); Working on it
-        void PutChar(char chr, unsigned int xOff, unsigned int yOff);
+        void Print(const char* str);
+        void Print(const char* str, uint64_t formatInt);
         void putPix(uint64_t X, uint64_t Y, uint64_t Color);
+        void PutChar(char Char, unsigned int XOffset, unsigned int YOffset);
 };
+
+#if MAPPLE_DEBUG != 0
+    #define DebugPrint(console,...) console.Print(__VA_ARGS__)
+#else
+    #define DebugPrint(console,...)
+#endif
