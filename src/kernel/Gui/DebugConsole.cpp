c05f60285f0cef8736c2c37f224233c02e65d9b5
platinumTypeC <randomeeerandom@gmail.com>
2022-02-28 21:30:25 +0530

    Implemented the Print function for a debug and error checking

--- src/kernel/Gui/DebugConsole.cpp
+++ src/kernel/Gui/DebugConsole.cpp
@@ -7,8 +7,43 @@ DebugConsole::DebugConsole(Framebuffer_t* frmaebuffer, PSF1_FONT_t* NewPSF1Font)
     CursorPosition = {10, 10};
 };
 
-// void DebugConsole::Print(const char* str){    
-// };
+void DebugConsole::PutChar(char Char, uint32_t XOffset, uint32_t YOffset) {
+    char* fontPtr = (char*)PSF1Font->glyphBuffer + (Char * PSF1Font->psf1_Header->charsize);
+    for(unsigned long Y = YOffset; Y < YOffset+16; Y++) {
+        for(unsigned long X = XOffset; X < XOffset+8; X++) {
+            if ((*fontPtr & (0b10000000 >> (X - XOffset))) > 0){
+                putPix(X, Y, Color);
+            }
+        }
+        fontPtr++;
+    }
+}
+
+void DebugConsole::Print(const char* str, uint64_t formatInt){ 
+    char* fmt = (char*)str;
+    while (*fmt != 0) {
+        PutChar(*fmt, CursorPosition.X, CursorPosition.Y);
+        CursorPosition.X += 8;
+        if (CursorPosition.X +8 > TargetFramebuffer->Width) {
+            CursorPosition.X = 0;
+            CursorPosition.Y += 16;
+        }
+        fmt++;
+    }    
+};
+
+void DebugConsole::Print(const char* str){
+    char* fmt = (char*)str;
+    while (*fmt != 0) {
+        PutChar(*fmt, CursorPosition.X, CursorPosition.Y);
+        CursorPosition.X += 8;
+        if (CursorPosition.X +8 > TargetFramebuffer->Width) {
+            CursorPosition.X = 0;
+            CursorPosition.Y += 16;
+        }
+        fmt++;
+    }    
+};
 
 void DebugConsole::putPix(uint64_t X, uint64_t Y, uint64_t Color){
     *(uint32_t*)((uint64_t)TargetFramebuffer->BaseAddress + (X*4) + (Y * TargetFramebuffer->PixelsPerScanLine * 4)) = Color;
