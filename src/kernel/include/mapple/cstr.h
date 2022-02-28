11df9aa3471de51311ccc4645717e67531a8b137
platinumTypeC <randomeeerandom@gmail.com>
2022-02-28 21:34:15 +0530

    Minor Changes

--- src/kernel/include/mapple/cstr.h
+++ src/kernel/include/mapple/cstr.h
@@ -9,5 +9,3 @@ const char* to_hstring(uint16_t value);
 const char* to_hstring(uint8_t value);
 const char* to_string(double value, uint8_t decimalPlaces);
 const char* to_string(double value);
-#pragma once
-#include <stdint.h>
