9efbfcfaba75b310a66b08b070a5c73c0f956b2b
platinumTypeC <randomeeerandom@gmail.com>
2022-03-01 21:24:55 +0530

    Preparing Memory Function is empty

---
+++ src/kernel/Memory/Memory.cpp
@@ -0,0 +1,7 @@
+#include <mapple/Memory.h>
+#include <mapple/types.h>
+#include <mapple/Gui.h>
+
+void PrepareMemory(BootInfo_t* boot_info){
+    DebugPrint("Preparing Memory...\n");
+}
--- src/kernel/include/mapple/Memory.h
+++ src/kernel/include/mapple/Memory.h
@@ -1,2 +1,7 @@
 #pragma once
 #include <mapple/Memory/GDT.h>
+#include <mapple/Memory/Paging.h>
+#include <mapple/Memory/PageFameAllocator.h>
+#include <mapple/types.h>
+
+void PrepareMemory(BootInfo_t* boot_info);
