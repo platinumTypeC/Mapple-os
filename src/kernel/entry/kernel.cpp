8bbf050836bd4b3489e438d7c12a6bc1873f08f7
platinumTypeC <randomeeerandom@gmail.com>
2022-03-01 21:23:08 +0530

    Seting GlobalConsole and seting Memory Map

--- src/kernel/entry/kernel.cpp
+++ src/kernel/entry/kernel.cpp
@@ -5,21 +5,24 @@
 #include <mapple/cstr.h>
 #include <cstddef>
 
+DebugConsole* GloballConsole;
+
 extern "C" uint64_t kernel_main(
     BootInfo_t* boot_info
 ){
+    clearScreen(boot_info->frameBuffer);
+
+    DebugConsole console = DebugConsole(&boot_info->frameBuffer, &boot_info->defaultFont);
+    GloballConsole = &console;
+    DebugPrint("\n");
+
     GDTDescriptor DescTableItem;
     DescTableItem.Size = sizeof(GDT) - 1;
     DescTableItem.Offset = (uint64_t)&DefaultGDT;
 
     LoadGDT(&DescTableItem);
 
-    clearScreen(boot_info->frameBuffer);
-
-    DebugConsole console = DebugConsole(&boot_info->frameBuffer, &boot_info->defaultFont);
-    console.Print("Print Service");
-    
-    asm("hlt");
+    PrepareMemory(boot_info);
 
     return 0;
 };
