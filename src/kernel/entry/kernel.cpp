dfa1a43791f72ab2bd2e04c543e72528f6dd4d6e
platinumTypeC <randomeeerandom@gmail.com>
2022-03-02 20:17:15 +0530

    Started preparing for Memory

--- src/kernel/entry/kernel.cpp
+++ src/kernel/entry/kernel.cpp
@@ -7,6 +7,23 @@
 
 DebugConsole* GloballConsole;
 
+void PrepareMemory(BootInfo_t* boot_info){
+    DebugPrint("Preparing Memory...\n");
+    
+    uint64_t mMapEntries = boot_info->memoryMap.mMapSize / boot_info->memoryMap.descSize;
+
+    PageFrameAllocator Allocator;
+
+    GlobalAllocator = &Allocator;
+
+    DebugPrint("Starting to read EfiMemoryMap\n");
+    GlobalAllocator->ReadEFIMemoryMap(boot_info->memoryMap.mMap, boot_info->memoryMap.mMapSize, boot_info->memoryMap.descSize);
+    DebugPrint("Done reading EfiMemoryMap\n");
+
+    uint64_t kernelSize = (uint64_t)&kernel_start - (uint64_t)&kernel_end;
+    uint64_t kernelPages = (uint64_t)kernelSize / 4096 + 1;
+}
+
 extern "C" uint64_t kernel_main(
     BootInfo_t* boot_info
 ){
@@ -23,6 +40,10 @@ extern "C" uint64_t kernel_main(
     LoadGDT(&DescTableItem);
 
     PrepareMemory(boot_info);
+
+    DebugPrint("Done..\n");
+
+    asm("hlt");
 
     return 0;
 };
