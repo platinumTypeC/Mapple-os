607086d9aca2046f4e605c6dd37e388768c06ee8
platinumTypeC <randomeeerandom@gmail.com>
2022-03-06 16:48:56 +0530

    Small restructure

--- src/kernel/include/mapple/Memory.h
+++ src/kernel/include/mapple/Memory.h
@@ -2,8 +2,8 @@
 #include <mapple/Memory/GDT.h>
 #include <mapple/Memory/Paging/Paging.h>
 #include <mapple/Memory/Heap.h>
-#include <mapple/Memory/IDT.h>
 #include <mapple/types.h>
 
+void PrepareMemory(BootInfo_t* bootInfo);
 uint64_t GetMemorySize(EFI_MEMORY_DESCRIPTOR* MemoryMapFirstDescriptor, uint64_t MemoryMapEntries, uint64_t MemoryMapDescriptorSize);
 void Memset(void* Start, uint8_t Value, uint64_t Num);
