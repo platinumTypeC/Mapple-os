350359373068b4e84651784d6ee293e67fc0393e
platinumTypeC <randomeeerandom@gmail.com>
2022-03-15 22:26:56 +0530

    Added Bitmap to PageFrameAlloc

--- src/kernel/include/mapple/Memory/Paging/PageFrameAllocator.h
+++ src/kernel/include/mapple/Memory/Paging/PageFrameAllocator.h
@@ -1,7 +1,38 @@
 #pragma once
 #include <mapple/types.h>
-#include <mapple/Bitmap.h>
-#include <mapple/Memory.h>
+
+
+class Bitmap_t{
+    public:
+    size_t Size;
+    uint8_t* Buffer;
+    bool operator[](uint64_t index){
+        return Get(index);
+    };
+    bool Set(uint64_t index, bool value){
+        if (index > Size * 8) return false;
+        uint64_t byteIndex = index / 8;
+        uint8_t bitIndex = index % 8;
+        uint8_t bitIndexer = 0b10000000 >> bitIndex;
+        Buffer[byteIndex] &= ~bitIndexer;
+        if (value){
+            Buffer[byteIndex] |= bitIndexer;
+        }
+        return true;
+    };
+    bool Get(uint64_t index){
+        if (index > Size * 8) return false;
+        uint64_t byteIndex = index / 8;
+        uint8_t bitIndex = index % 8;
+        uint8_t bitIndexer = 0b10000000 >> bitIndex;
+        if ((Buffer[byteIndex] & bitIndexer) > 0){
+            return true;
+        }
+        return false;
+    };
+};
+
+#define Bitmap Bitmap_t
 
 class PageFrameAllocator {
     public:
