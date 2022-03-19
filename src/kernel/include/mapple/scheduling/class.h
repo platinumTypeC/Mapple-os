ac0e16a5e3abd7e36d0b878b8b996d6199f1792a
platinumTypeC <randomeeerandom@gmail.com>
2022-03-19 19:55:25 +0530

    Scheduler structure

--- src/kernel/include/mapple/scheduling/class.h
+++ src/kernel/include/mapple/scheduling/class.h
@@ -1,6 +1,16 @@
 #pragma once
 #include <mapple/scheduling/Proccess.h>
 
-class scheduling{
-    Proccess* p;
+class Scheduler{
+    public:
+        Proccess* TopStack;
+        Proccess* MiddleStack;
+        Proccess* BottomStack;
+        Proccess* WaitingStack;
+
+        uint64_t time;
+        const uint64_t timeBlock = 1000;
+
+        void tick();
+        void addProcess(Proccess process);
 };
