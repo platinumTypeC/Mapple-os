7ac156747fda20ffa48983e3dce753a1f6c7e1e4
platinumTypeC <randomeeerandom@gmail.com>
2022-03-21 18:15:39 +0530

    Using namespace instead of class

--- src/kernel/include/mapple/scheduling/class.h
+++ src/kernel/include/mapple/scheduling/class.h
@@ -1,16 +1,7 @@
 #pragma once
 #include <mapple/scheduling/Proccess.h>
 
-class Scheduler{
-    public:
-        Proccess* TopStack;
-        Proccess* MiddleStack;
-        Proccess* BottomStack;
-        Proccess* WaitingStack;
-
-        uint64_t time;
-        const uint64_t timeBlock = 1000;
-
-        void tick();
-        void addProcess(Proccess process);
-};
+namespace Scheduler {
+    void addProcess(uint8_t pid);
+    void removeProcess(uint8_t pid);
+}
--- src/kernel/scheduling/class.cpp
+++ src/kernel/scheduling/class.cpp
@@ -1,6 +1,25 @@
-#include <mapple/scheduling/class.h>
+#include <mapple/scheduling.h>
+#include <mapple/Memory.h>
+#include <mapple/Gui.h>
 
+namespace Scheduler {
+    namespace 
+    {
+        LIST_INIT(TopStack);
+        LIST_INIT(MiddleStack);
+        LIST_INIT(BottomStack);
+        LIST_INIT(WaitingStack);
+    }
 
-void Scheduler::addProcess(Proccess process)
-{
+    void addProcess(uint8_t pid)
+    {
+        Proccess process;
+        process.pid = pid;
+
+        list_add(&process.node, &TopStack);
+    };
+
+    void removeProcess(uint8_t pid)
+    {
+    };
 }
