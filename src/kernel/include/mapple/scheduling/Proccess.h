6b872351e77f33fb2a13d2be67576d327a270a55
platinumTypeC <randomeeerandom@gmail.com>
2022-03-21 19:46:46 +0530

    Process start pointer added and restructure

--- src/kernel/include/mapple/scheduling/Proccess.h
+++ src/kernel/include/mapple/scheduling/Proccess.h
@@ -5,8 +5,8 @@
 class Proccess{
     public:
     ll_t node;
-    uint8_t pid;
-    uint64_t importance;
+    uint16_t pid;
+    uint8_t importance;
     uint8_t isWaiting;
-    void wait();
+    void* start;
 };
